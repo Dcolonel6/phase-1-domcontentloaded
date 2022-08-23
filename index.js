389b1470941bda4e1cce75f02e982585336050ba
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-23 13:04:44 +0300

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded',() =>{
+    ChangeText(document.getElementById('text'))
+
+})
+
+function ChangeText(domElement){
+    domElement.textContent = "This is really cool!"
+}
